05830d09041d9eb51236373adaefc1b67f9fb409
zijiren233 <pyh1670605849@gmail.com>
2025-08-12 17:19:47 +0800

    fix: pay ci lint

--- service/pay/main.go
+++ service/pay/main.go
@@ -10,12 +10,12 @@ func main() {
 	// cert, err := tls.LoadX509KeyPair("./key/pay.pem", "./key/pay.key")
 	// if err != nil {
 	//	log.Fatalf("failed to load certificates and keys: %v", err)
-	//}
+	// }
 	// cfg := &tls.Config{Certificates: []tls.Certificate{cert}}
 	//
 	// listener, err := tls.Listen("tcp", ":8080", cfg)
-	//if err != nil {
+	// if err != nil {
 	//	log.Fatal(err)
-	//}
-	//defer listener.Close()
+	// }
+	// defer listener.Close()
 }
